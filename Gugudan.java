d85a5f07e0ba4ea014466a4e7e0115e06283bdde
CSN-na22 <csa2676@naver.com>
2020-09-08 00:58:34 +0900

    구구단-무식하게 프린트해서 2단, 3단 세어보고 4단에서 변수를 이용해서 출력해보고 5단에서는 사용자에게 입력받고 입력받은수를 변수에 저장해서 출력하기 6단 7단은 while문과 for문으로 8단,9단은 사용자가 9이하 2이상 입력하도록 조건식 까지

---
+++ Gugudan.java
@@ -0,0 +1,92 @@
+import java.util.Scanner;
+
+public class Gugudan {
+    public static void main(String[] args) {
+        //8, 9단
+        System.out.println("구구단 줄 출력할 단은?: ");
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+        System.out.println("사용자가 입력한 값: " + number);
+
+        if (number < 2) {
+            System.out.println("값을 잘 못 입력했습니다");
+
+        } else if (number > 9) {
+
+            System.out.println("값을 잘 못 입력했습니다");
+
+        }
+        else {
+            for(int i =1; i <10; i++)
+                System.out.println(number *i);
+        }
+
+
+        //6단
+//        int i = 1;
+//        while(i < 10){
+//            System.out.println(6 * i);
+//            i = i + 1;
+//        }
+//        //?단
+//        for(int j = 1; j < 10; j++) {
+//            System.out.println(7*j);
+//        }
+
+        //2단
+//        System.out.println("2단");
+//        System.out.println(2 * 1);
+//        System.out.println(2 * 2);
+//        System.out.println(2 * 3);
+//        System.out.println(2 * 4);
+//        System.out.println(2 * 5);
+//        System.out.println(2 * 6);
+        //3단
+        //System.out.println("3단");
+//      System.out.println(3 * 1);
+//      System.out.println(3 * 2);
+//      System.out.println(3 * 3);
+//      System.out.println(3 * 4);
+//      System.out.println(3 * 5);
+//      System.out.println(3 * 6);
+//      System.out.println(3 * 9);
+//      System.out.println(3 * 7);
+//      System.out.println(3 * 8);
+
+        //4단
+//        System.out.println("4단");
+//        int result = 4 * 1;
+//        System.out.println(result);
+//        result = 4 * 2;
+//        System.out.println(result);
+//        result = 4 * 3;
+//        System.out.println(result);
+//        result = 4 * 4;
+//        System.out.println(result);
+//        result = 4 * 5;
+//        System.out.println(result);
+//        result = 4 * 6;
+//        System.out.println(result);
+//        result = 4 * 7;
+//        System.out.println(result);
+//        result = 4 * 8;
+//        System.out.println(result);
+//        result = 4 * 9;
+//        System.out.println(result);
+
+//        System.out.println("구구단 줄 출력할 단은?: ");
+//        Scanner scanner = new Scanner(System.in);
+//        int number = scanner.nextInt();
+//        System.out.println(number * 1);
+//        System.out.println(number * 2);
+//        System.out.println(number * 3);
+//        System.out.println(number * 4);
+//        System.out.println(number * 5);
+//        System.out.println(number * 6);
+//        System.out.println(number * 7);
+//        System.out.println(number * 8);
+//        System.out.println(number * 9);
+
+    }
+}
+
