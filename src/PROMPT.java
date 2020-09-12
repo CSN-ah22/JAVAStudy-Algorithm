29b9e7cf74054cc8e19b94bab900755857a06ec9
CSN-na22 <csa2676@naver.com>
2020-09-13 03:12:09 +0900

    더 정교한 달력 찍기 (빈 공간 만들어주기) 12강 엽습 코드

--- src/PROMPT.java
+++ src/PROMPT.java
@@ -1,30 +1,47 @@
-import java.util.Calendar;
 import java.util.Scanner;
 
 public class PROMPT {
-    private final static String PROMPT = ("calendar>");
+//     week 요일명
+//     return 0 ~ 6 (0 =Sunday, 6 = Saturday)
+    public int parseDay(String week) {
+        if      (week.equals ("su")) { return 0; }
+        else if (week.equals ("mo")) { return 1; }
+        else if (week.equals ("tu")) { return 2; }
+        else if (week.equals ("wd")) { return 3; }
+        else if (week.equals ("th")) { return 4; }
+        else if (week.equals ("fr")) { return 5; }
+        else if (week.equals ("sa")) { return 6; }
+            return 0;
+    }
 
-    public static void runPrompt() {
+    public void runPrompt() {
         Scanner scanner = new Scanner(System.in);
         calendarPractice cal = new calendarPractice();
 
-        int month = 1;
-        int year = 2020;
+        int month ;
+        int year ;
+        int weekday ;
 
         while (true) {
-            System.out.println("년도를 입력하세요");
-            System.out.println("YEAR>");
+            System.out.println("년도를 입력하세요 (exit: -1");
+            System.out.print("YEAR>");
             year = scanner.nextInt();
+            if( year == -1)
+                break;
+
             System.out.println("달을 입력하세요");
             System.out.print("MONTH>");
             month = scanner.nextInt();
-            if (month == -1) {
-                break;
-            }
-            if (month > 12) {
+
+            System.out.println("첫째 날의 요일을 입력하세요(su, mo, tu, wd, th, fr, st");
+            String str_weekday = scanner.next();
+            weekday = parseDay(str_weekday);
+
+            if (month > 12 || month < -1) {
+                System.out.println("잘못된 입력값입니다.");
                 continue;
             }
-            cal.prints(year, month);
+            cal.prints(year, month, weekday);
 
 
         }
--- src/calendarPractice.java
+++ src/calendarPractice.java
@@ -1,39 +1,57 @@
-
-
 public class calendarPractice {
     private static final int[] mmd = {31, 28, 30, 31, 30, 31, 30, 31, 30, 31, 30, 31};
-    private static final int[] LEAP_MAX_DAYS = {31, 29, 30, 31, 30, 31, 30, 31, 30, 31, 30, 31};
+    private final int[] LEAP_MAX_DAYS = {31, 29, 30, 31, 30, 31, 30, 31, 30, 31, 30, 31};
 
-    public boolean isLeapYear(int year){
-        if ( year % 4 ==0 &&( year % 100 !=0 || year % 400 ==0)) {
-            return true;
-        } else {
-            return false;
-        }
+    public boolean isLeapYear(int year) {
+        return year % 4 == 0 && (year % 100 != 0 || year % 400 == 0);
     }
 
-    public int maxDays(int year,int month) {
+    public int maxDays(int year, int month) {
         if (isLeapYear(year)) {
-            return  LEAP_MAX_DAYS[month-1];
-        }else {
+            return LEAP_MAX_DAYS[month - 1];
+        } else {
             return mmd[month - 1];
         }
     }
 
-    public void prints(int year,int month) {
+    public void prints(int year, int month, int weekday) {
         System.out.printf(" <<%4d년 %3d월>>\n", year, month);
         System.out.println(
-                        "SU MO TU WE TY FR SA\n");
+                "SU MO TU WD TH FR SA");
 
+        for (int i = 0; i < weekday; i++) {
+            System.out.print("   ");
+        }
         int days = maxDays(year, month);
+        int count = 7 - weekday;
+        int delim = (count < 7) ? count : 0;
 
-        for (int i = 1; i <= days; i++) {
-            System.out.printf("%3d",i);
-            if (i % 7 == 0) {
-                System.out.println();
-            }
+       /* ↑요거랑 같음
+       if (count < 7) {
+            delim =count;
+        }else {
+            delim = 0;
+        }
+*/
+        //print first line
+        for (int i = 1; i <= count; i++) {
+            System.out.printf("%3d", i);
         }
         System.out.println();
+
+        //print from second line to last
+        count++;
+        for (int i = count; i <= days; i++) {
+            System.out.printf("%3d", i);
+            if (i % 7 == delim) {
+                System.out.println();
+            }
+
+
+        }
+        System.out.println();
+        System.out.println();
+        System.out.println(" ");
 //                        "일	월	화	수	목	금	토\n" +
 //                        "30	31	1	2	3	4	5\n" +
 //                        "6	7	8	9	10	11	12\n" +
