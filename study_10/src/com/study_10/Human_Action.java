b82a16bd3648da7ced207372a02abfbeadf2234d
CSN-ah22 <70833455+CSN-ah22@users.noreply.github.com>
2021-04-29 15:15:34 +0900

    Delete study_10 directory

--- study_10/src/com/study_10/Human_Action.java
+++
@@ -1,8 +0,0 @@
-package com.study_10;
-
-public interface Human_Action {
-    public void Stand();
-    public void Walk(int Step);
-    public void Eating(String food);
-    public void sleep();
-}
